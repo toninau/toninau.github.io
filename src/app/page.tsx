628f23bf3b87b7d53081e7c8de39eb4b4e66e1b9
Toni Naumanen <toni.naumis@gmail.com>
2024-09-15 13:33:07 +0300

    Add test page to test linking and navigating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <p>Hello world!!!</p>
+      <Link href="/test">Go to test page</Link>
     </div>
   );
 }
---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Test() {
+  return (
+    <div>
+      <p>Hello from test page!</p>
+      <Link href="/">Return to home page</Link>
+    </div>
+  )
+}
