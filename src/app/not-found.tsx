f9919bb40dae05fc55038ba41462b1a892e82def
Toni Naumanen <toni.naumis@gmail.com>
2024-11-03 18:59:22 +0200

    Update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,24 @@
+import ArrowReturnLeft from '@/components/icons/ArrowReturnLeft';
 import Link from '@/components/Link';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center">
+      <div aria-hidden="true" className="flex select-none flex-col items-center justify-center">
+        <span className="text-nowrap text-[14rem] leading-tight tracking-tighter text-stone-200">
+          : (
+        </span>
+        <span className="font-mono text-4xl tracking-tighter text-stone-200">404</span>
+      </div>
+      <h1 className="my-8 text-center text-4xl font-semibold tracking-tighter text-stone-900 dark:text-white">
+        Page not found!
+      </h1>
+      <Link
+        href={'/'}
+        className="mb-8 inline-flex items-center gap-2 rounded-lg bg-button p-2 text-base font-medium tracking-tighter text-secondary hover:bg-button-hover active:bg-button-active"
+      >
+        <ArrowReturnLeft /> Return to front page
+      </Link>
     </div>
   );
 }
