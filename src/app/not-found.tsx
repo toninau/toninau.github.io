0ff5fd6fdeca037637a786d366a97a8a40236117
Toni Naumanen <toni.naumis@gmail.com>
2024-11-05 20:11:43 +0200

    Add missing dark theme colors to 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,11 +4,12 @@ import Link from '@/components/Link';
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div aria-hidden="true" className="flex select-none flex-col items-center justify-center">
-        <span className="text-nowrap text-[14rem] leading-tight tracking-tighter text-stone-200">
-          : (
-        </span>
-        <span className="font-mono text-4xl tracking-tighter text-stone-200">404</span>
+      <div
+        aria-hidden="true"
+        className="flex select-none flex-col items-center justify-center text-stone-200 dark:text-stone-200/10"
+      >
+        <span className="text-nowrap text-[14rem] leading-tight tracking-tighter">: (</span>
+        <span className="font-mono text-4xl tracking-tighter">404</span>
       </div>
       <h1 className="my-8 text-center text-4xl font-semibold tracking-tighter text-stone-900 dark:text-white">
         Page not found!
