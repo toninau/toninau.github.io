989202cfedcb8cecb10cd0987848ba8e7a6a13a8
Toni Naumanen <toni.naumis@gmail.com>
2024-12-23 15:02:19 +0200

    Change return to front page link styling

--- src/components/ReturnToFrontPageLink.tsx
+++ src/components/ReturnToFrontPageLink.tsx
@@ -5,9 +5,10 @@ export default function ReturnToFrontPageLink() {
   return (
     <Link
       href={'/'}
-      className="inline-flex items-center gap-2 rounded-lg bg-button p-2 text-base font-medium tracking-tighter text-secondary hover:bg-button-hover active:bg-button-active"
+      className="inline-flex items-center gap-1 rounded-lg bg-button p-2 text-base font-medium tracking-tighter text-secondary hover:bg-button-hover active:bg-button-active"
     >
-      <ArrowReturnLeft /> Return to front page
+      <ArrowReturnLeft />
+      <span>Return to front page</span>
     </Link>
   );
 }
